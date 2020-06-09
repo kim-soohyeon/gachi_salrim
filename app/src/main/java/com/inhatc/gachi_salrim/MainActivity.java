380809c71a941b5c6676ab1e6c6c36995fc9c07e
LeeChaeng <htijl@naver.com>
2020-06-09 21:44:55 +0900

    firebase 연동 test Data insert 완료

--- app/src/main/java/com/inhatc/gachi_salrim/MainActivity.java
+++ app/src/main/java/com/inhatc/gachi_salrim/MainActivity.java
@@ -1,23 +1,65 @@
 package com.inhatc.gachi_salrim;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
 
     private DatabaseReference mDatabase;    //firebase DB 정보 선언
+    EditText tbx_test;
+    Button btn_test;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        tbx_test = findViewById(R.id.tbx_test);
+        btn_test = findViewById(R.id.btn_test);
 
         mDatabase = FirebaseDatabase.getInstance().getReference();  //firebase 정의
+
+        btn_test.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String getUserName = tbx_test.getText().toString();
+                writeData(getUserName,"1");
+            }
+        });
+
+
+    }
+    private void writeData(String username, String userId) {
+        System.out.println(username);
+        System.out.println(userId);
+
+        mDatabase.child("users").child(userId).setValue(username)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        // Write was successful!
+                        Toast.makeText(MainActivity.this, "저장을 완료했습니다.", Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        // Write failed
+                        Toast.makeText(MainActivity.this, "저장을 실패했습니다.", Toast.LENGTH_SHORT).show();
+                    }
+                });
 
     }
 }

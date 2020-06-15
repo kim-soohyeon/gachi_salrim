da437aeb5346f83c80ee14ffe31d5f260d834cd9
kimyeji203 <kimyeji203@naver.com>
2020-06-15 21:57:15 +0900

    Firebase 버전 맞춤 - 종속성 동기화

--- app/src/main/java/com/inhatc/gachi_salrim/LoginActivity.java
+++ app/src/main/java/com/inhatc/gachi_salrim/LoginActivity.java
@@ -1,4 +1,41 @@
 package com.inhatc.gachi_salrim;
 
-public class LoginActivity {
+import android.app.Activity;
+import android.content.ContentProviderClient;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class LoginActivity extends Activity {
+
+    public TextView btnGotoSingup;
+    public Button btnLogin;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        // 회원가입 버튼
+        btnGotoSingup = findViewById(R.id.btnGotoSignup);
+        btnGotoSingup.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Intent intent = new Intent(LoginActivity.this,SignupActivity.class);
+                startActivity(intent); // 회원가입 화면으로 이동
+            }
+        });
+
+        // 로그인 버튼
+        btnLogin = findViewById(R.id.btnLogin);
+        btnLogin.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                // 로그인 시도
+            }
+        });
+    }
 }
--- app/src/main/java/com/inhatc/gachi_salrim/SignupActivity.java
+++ app/src/main/java/com/inhatc/gachi_salrim/SignupActivity.java
@@ -1,4 +1,30 @@
 package com.inhatc.gachi_salrim;
 
-public class SignupActivity {
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.EditText;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthException;
+
+public class SignupActivity extends Activity {
+
+    EditText editName;
+    EditText editId;
+    EditText editPw;
+    EditText editPwChk;
+    EditText editEmail;
+    EditText editPhone;
+    EditText editErea;
+
+    private FirebaseAuth firebaseAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+    }
 }

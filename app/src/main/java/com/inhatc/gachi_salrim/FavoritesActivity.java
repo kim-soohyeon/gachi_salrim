1ea92c8824685eda99ab7c2ee36cff688a565d18
LeeChaeng <htijl@naver.com>
2020-06-24 13:12:07 +0900

    Favorites (즐겨찾기) 시 이미지 로드

---
+++ app/src/main/java/com/inhatc/gachi_salrim/FavoritesActivity.java
@@ -0,0 +1,14 @@
+package com.inhatc.gachi_salrim;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class FavoritesActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_favorites);
+    }
+}
